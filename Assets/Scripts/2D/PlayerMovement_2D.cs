7068ef6a70fc7e8faeb5e521bb12168103e47480
Kamal Azizov <70916892+Rungrukuk@users.noreply.github.com>
2025-02-22 11:19:17 +0400

    Initial commit

---
+++ Assets/Scripts/2D/PlayerMovement_2D.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerMovement_2D : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private SpriteRenderer spriteRenderer;
+
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement = movement.normalized;
+        if (movement.x > 0)
+            spriteRenderer.flipX = false;
+        else if (movement.x < 0)
+            spriteRenderer.flipX = true;
+
+        if (movement.x != 0 || movement.y != 0)
+            animator.SetBool("isRunning", true);
+        else
+            animator.SetBool("isRunning", false);
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movement * moveSpeed;
+    }
+}
